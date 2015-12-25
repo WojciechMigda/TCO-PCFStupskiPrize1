3df0edead4478d9564ab8d1d544d49eceb7e732e
Wojciech Migda <wojtek.golf@interia.pl>
2015-12-25 23:53:30 +0100

    cleanup

--- src/cell_patches_kmeans.sh
+++ src/cell_patches_kmeans.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./cell_patches.py \
+./cell_patches_kmeans.py \
   -i ../../data/training.csv \
-  -o cell_patches.csv \
+  -o cell_patches_kmeans.csv \
   --patch-size=16 \
   --num-patches=80 \
   --max-pois=5000
