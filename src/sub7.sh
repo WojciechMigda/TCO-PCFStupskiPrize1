25cbad7e582d7b2a26d15ab44cf3237bf9d105f4
Wojciech Migda <wojtek.golf@interia.pl>
2015-12-26 00:00:00 +0100

    sub 6&7 scripts

---
+++ src/sub7.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+NPOI=5000
+FEATDIR=../../data
+FEATDIR=.
+
+./vq.py -i ../../data/training.csv -o DBSCAN_cart_${NPOI}_train_features.csv -N ${NPOI}
+
+./vq.py -i ../../data/test.csv -o DBSCAN_cart_${NPOI}_test_features.csv -N ${NPOI}
+
+./clf_ert.py \
+  --in-y-train-csv ../../data/training.csv \
+  --in-test-labels-csv ../../data/test.csv \
+  --in-test-feat-csv ${FEATDIR}/DBSCAN_cart_${NPOI}_test_features.csv \
+  --in-train-feat-csv ${FEATDIR}/DBSCAN_cart_${NPOI}_train_features.csv \
+  -o submission07.csv
